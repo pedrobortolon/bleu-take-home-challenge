a2dcaf42abe6ebc8e706a2c1f0aef8bd6d4c550c
Pedro Bortolon <pedro.b.p.cunha@gmail.com>
2025-04-16 21:53:26 -0300

    create global stats page

---
+++ apps/web/src/app/global-stats/page.tsx
@@ -0,0 +1,9 @@
+import MintNFTButton from '@/components/mint-nft-button';
+import NFTInfo from '@/components/nft-info';
+import { placeholder } from '@bleu-builders/tech-challenge-ui';
+
+export default function Home() {
+  return (
+    <p>whatever</p>
+  );
+}
